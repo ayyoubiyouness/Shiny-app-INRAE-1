34569207b6c8412e488dde14c0de4898cbbe224e
Ayyoubi <114993701+ayyoubiyouness@users.noreply.github.com>
2023-06-19 02:38:03 -0800

    Update app.R

--- app.R
+++ app.R
@@ -1,11 +1,11 @@
-# Load packages 
+library(markdown)
 library(shiny)
 library(ggplot2)
 library(readxl)
 library(ggcorrplot)
+library(tidyverse)
 
 
-# Get the data
 setwd("C:/Users/Youness/Desktop/R project/stage/shiny/inraeshiny")
 
 
@@ -42,86 +42,131 @@ colnames(data) <- c(
 a <- c("Forage_available", "Grain_available", "Grassland_available", "Rangeland_available",
        "Rangeland_ingest", "Grassland_ingest", "Grain_ingest", "Forage_ingest", "cycle"  )
 
-# Define UI 
 
-ui <- fluidPage(
-  titlePanel("Interface et Visualisation de données de simulation : "),
-  
-  sidebarLayout(
+
+ui <- navbarPage("INRAE : Interface de visualisation ",
+                 tabPanel("Courbe",
+                          sidebarLayout(
+                            sidebarPanel(
+                              h4("Choix des paramètres", align = "center"),
+                              
+                              # Select variable for y-axis
+                              p("Choisir l'axe des ordonnées"),
+                              selectInput(
+                                inputId = "xcol",
+                                label = "Y-axis:",
+                                choices = a,
+                                selected = "Forage_available"
+                              ),
+                              p("Choisir l'axe des abscisses"),
+                              selectInput(
+                                inputId = "ycol",
+                                label = "X-axis:",
+                                choices = a,
+                                selected = "Forage_ingest"
+                              ),
+                              numericInput('clusters', 'Pour répartir les données', 2, min = 1, max = 9),
+                              br(),
+                              p("Pour Afficher la matrice de corrélation, cliquez sur la boutton"),
+                              br(),
+                              actionButton("go", "Correlation")
+                              
+                            ),
+                            mainPanel(
+                              h4("Représentation des graphes", align ="center"),
+                              h4("Nuage de points", style = "color:blue"),
+                              plotOutput('plot1'),
+                              h4("Matrice de corrélation", style = "color:blue"),
+                              plotOutput(outputId = "correlation")
     
-    # Inputs: Select variables to plot
-    sidebarPanel(
-      # img( src = "www/inraeimage.png" , height = 40, width = 40),
-      
-      h4("Choix des paramètres", align = "center"),
-      
-      # Select variable for y-axis
-      p("Choisir l'axe des ordonnées"),
-      selectInput(
-        inputId = "y",
-        label = "Y-axis:",
-        choices = a,
-        selected = "Forage_available"
-      ),
-      # Select variable for x-axis
-      p("Choisir l'axe des abscisses"),
-      selectInput(
-        inputId = "x",
-        label = "X-axis:",
-        choices = a,
-        selected = "Forage_ingest"
-      ),
-      br(),
-      p("Pour Afficher la matrice de corrélation, cliquez sur la boutton"),
-      br(),
-      actionButton("go", "Correlation"),
-      
-    ),
     
-    # Output: Show scatterplot
-    mainPanel(
-      h4("Représentation des graphes", align ="center"),
-      
-      h4("Nuage de points", style = "color:blue"),
-      plotOutput(outputId = "scatterplot"),
-      
-      h4("Matrice de corrélation", style = "color:blue"),
-      plotOutput(outputId = "correlation")
-      
-    )
-  )
+                            )
+                          )
+                 ),
+                 tabPanel("Résumé",
+                          verbatimTextOutput("summary")
+                 ),
+                 navbarMenu("Indicateur",
+                            tabPanel("table",
+                                     DT::dataTableOutput("table")
+                            ),
+                            tabPanel("Indicateurs de disponibilité des aliments ",
+                                     sidebarLayout(
+                                       sidebarPanel("sidebar panel"),
+                                       mainPanel("main panel")
+                                     )
+                                     ),
+                            tabPanel("Indicateurs de consommation d'aliments  ",
+                                     sidebarLayout(
+                                       sidebarPanel("sidebar panel"),
+                                       mainPanel("main panel")
+                                     )
+                            ),
+                            tabPanel("Indicateurs de production de la ferme ",
+                                     sidebarLayout(
+                                       sidebarPanel("sidebar panel"),
+                                       mainPanel("main panel")
+                                     )
+                            ),
+                            
+                            tabPanel("Indicateurs de performance du troupeau  ",
+                                     sidebarLayout(
+                                       sidebarPanel("sidebar panel"),
+                                       mainPanel("main panel")
+                                     )
+                            ),
+                            
+                            tabPanel("younes",
+                                     sidebarLayout(
+                                       sidebarPanel("sidebar panel"),
+                                       mainPanel("main panel")
+                                     ))
+                 )
 )
 
 
 
-# Define server 
-
 server <- function(input, output, session) {
-  output$scatterplot <- renderPlot({
-    ggplot(data = data, aes_string(x = input$x, y = input$y)) +
-      geom_point()
+  
+  selectedData <- reactive({
+    data[, c(input$xcol, input$ycol)]
+  })
+  
+  clusters <- reactive({
+    kmeans(selectedData(), input$clusters)
+  })
+  
+  output$plot1 <- renderPlot({
+    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+              "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+    
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData(),
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
+    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
   })
   
   randomVals <- eventReactive(input$go, {
     r <- cor(data)
     round(r, 2)
   })
+  
   output$correlation <- renderPlot({
-    ggcorrplot(randomVals(), lab = TRUE )
+    ggcorrplot(randomVals(), lab = TRUE)
   })
   
+  
+  
+  output$summary <- renderPrint({
+    summary(data)
+  })
+  
+  output$table <- DT::renderDataTable({
+    DT::datatable(data)
+  })
 }
 
-# Create a Shiny app object 
-
 shinyApp(ui = ui, server = server)
-
-# randomVals <- eventReactive(input$go, {
-#   runif(input$n)
-# })
-# 
-# output$plot <- renderPlot({
-#   hist(randomVals())
-# })
 
 
